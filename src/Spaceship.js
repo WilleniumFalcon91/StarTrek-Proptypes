2ca5a5355879afd2e3792878f236b1c93feec412
William Elliott <wcelliott1991@gmail.com>
2017-09-13 10:32:00 -0400

    adds spaceship and proptype

--- src/Spaceship.js
+++ src/Spaceship.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Spaceship extends Component {
+    static propTypes = {
+        name: PropTypes.string
+    };
+
     render() {
-        
+        const { name } = this.props;
+        return (
+            <div>
+                <h1>{name}</h1>
+            </div>
+        )
     }
 }
+
+export default Spaceship;
