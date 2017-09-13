281096d7218415e8ebf06a1abe300f91acc4eef7
William Elliott <wcelliott1991@gmail.com>
2017-09-13 13:49:24 -0400

    adds actions, store and reducer

---
+++ src/actions.js
@@ -0,0 +1,16 @@
+
+
+// {
+//     type: ADD_MEMBER,
+//     name: 'crusher',
+//     rank: 'chief medical officer',
+//     id: 12345
+// }
+
+const ADD_MEMBER = 'ADD_MEMBER';
+const BEAM_MEMBER = "BEAM_MEMBER";
+
+export default {
+    ADD_MEMBER,
+    BEAM_MEMBER,
+}
